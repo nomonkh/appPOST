7da0f137a90fcb9877658d645c05f37becfcf1bb
Номонжон Хошимов <khoshimov.nomonjon@loc.tashkent.uz>
2022-04-12 20:09:23 +0500

    ch

--- appAPI/appAPI/settings.py
+++ appAPI/appAPI/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 from datetime import timedelta
 from pathlib import Path
-
+import environ
+env = environ.Env()
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v839r41y#!cwn9g%81j*-ibt-p*+#jb@cc@-%1$#z8@*v_s_=!'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -128,11 +130,11 @@ DATABASES = {
         'OPTIONS': {
             'options': '-c search_path=django,controller_main'
         },
-        'NAME': 'local_db',
-        'USER': 'postgres',
-        'PASSWORD': '1',
-        'HOST': '127.0.0.1',
-        'PORT': '5432'
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT':  env('DB_PORT')
     }
 }
 
