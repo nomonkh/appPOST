99fa1785cd64a441c02e6d24fc30395e67fe9078
Номонжон Хошимов <khoshimov.nomonjon@loc.tashkent.uz>
2022-04-12 11:03:59 +0500

    models changes

--- appAPI/dxarid/models.py
+++ appAPI/dxarid/models.py
@@ -31,17 +31,17 @@ class Main(models.Model):
         db_table = 'dxarid\".\"main'
 
 class Products(models.Model):
-    lot_id = models.CharField(max_length=255)
-    art_id = models.IntegerField()
-    p_product_name =  models.CharField(max_length=255)
-    measure_gnk = models.CharField(max_length=255)
-    quantity = models.IntegerField()
-    one_price = models.IntegerField()
-    all_price = models.IntegerField()
-    descript = models.CharField(max_length=255)
-    tnvd_code = models.CharField(max_length=255)
-    expend_id = models.CharField(max_length=255)
-    plan_position_id = models.IntegerField()
+    lot_id = models.CharField(max_length=255, null=True)
+    art_id = models.IntegerField( null=True)
+    p_product_name =  models.CharField(max_length=255, null=True)
+    measure_gnk = models.CharField(max_length=255, null=True)
+    quantity = models.IntegerField(null=True)
+    one_price = models.IntegerField(null=True)
+    all_price = models.IntegerField(null=True)
+    descript = models.CharField(max_length=255, null=True)
+    tnvd_code = models.CharField(max_length=255, null=True)
+    expend_id = models.CharField(max_length=255, null=True)
+    plan_position_id = models.IntegerField(null=True)
     # user = models.ForeignKey(User, verbose_name='User', on_delete=models.CASCADE)
 
     def __str__(self):
